8c1ba5a743725c3918d25504fe18440da292d3f4
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-10-16 19:37:44 -0500

    commit 1

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,15 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+//Richard Padilla
+public class Empleado {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    String nombre, cargo;
+    double salario;
+    String fechaIngreso;
+
+    public Empleado(String nombre, String cargo, double salario, String fechaIngreso) {
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Salario: $" + salario);
+        System.out.println("Fecha de Ingreso: " + fechaIngreso);
+        System.out.println("------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Primer empleado
+        System.out.println("Ingrese los detalles del primer empleado:");
+        System.out.print("Nombre: ");
+        String nombre1 = scanner.nextLine();
+        System.out.print("Cargo: ");
+        String cargo1 = scanner.nextLine();
+        System.out.print("Salario: ");
+        double salario1 = scanner.nextDouble();
+        scanner.nextLine(); // Limpiar el buffer
+        System.out.print("Fecha de Ingreso (ej. 2022-01-15): ");
+        String fechaIngreso1 = scanner.nextLine();
+        Empleado empleado1 = new Empleado(nombre1, cargo1, salario1, fechaIngreso1);
+
+        //Segundo empleado
+        System.out.println("Ingrese los detalles del segundo empleado:");
+        System.out.print("Nombre: ");
+        String nombre2 = scanner.nextLine();
+        System.out.print("Cargo: ");
+        String cargo2 = scanner.nextLine();
+        System.out.print("Salario: ");
+        double salario2 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Fecha de Ingreso (ej. 2022-01-15): ");
+        String fechaIngreso2 = scanner.nextLine();
+        Empleado empleado2 = new Empleado(nombre2, cargo2, salario2, fechaIngreso2);
+
+        //Tercer empleado
+        System.out.println("Ingrese los detalles del tercer empleado:");
+        System.out.print("Nombre: ");
+        String nombre3 = scanner.nextLine();
+        System.out.print("Cargo: ");
+        String cargo3 = scanner.nextLine();
+        System.out.print("Salario: ");
+        double salario3 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Fecha de Ingreso (ej. 2022-01-15): ");
+        String fechaIngreso3 = scanner.nextLine();
+        Empleado empleado3 = new Empleado(nombre3, cargo3, salario3, fechaIngreso3);
+
+        System.out.println("Información de los empleados:");
+        empleado1.mostrarInformacion();
+        empleado2.mostrarInformacion();
+        empleado3.mostrarInformacion();
     }
 }
